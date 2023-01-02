379beb0f11787a936410810d5b76a3166a9ced81
kushal <kushalbosamiya13@gmail.com>
2023-01-02 15:55:52 +0530

    removed herosection

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <>
       <Navbar />
-      <HeroSection />
+      {/* <HeroSection /> */}
       <PerformanceSection />
       <AddonPerformance />
       <ProductSection />
