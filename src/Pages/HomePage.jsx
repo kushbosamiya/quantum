cff292cbbe31d47cf11296e7811fc5044325b741
kushal <kushalbosamiya13@gmail.com>
2023-01-04 08:48:13 +0530

    added framer motion for loading purpose

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Box } from "@chakra-ui/react";
 import Navbar from "../Components/NavBar";
 import HeroSection from "../Components/HeroSection";
 import PerformanceSection from "../Components/PerformanceSection/PerformanceSection";
@@ -8,18 +9,29 @@ import AboutSection from "../Components/AboutSection";
 import FooterSection from "../Components/FooterSection";
 import ContactSection from "../Components/ContactSection";
 import FooterSec from "../Components/FooterSec";
+
+import { AnimatePresence, motion } from "framer-motion";
 const HomePage = () => {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <PerformanceSection />
-      <AddonPerformance />
-      <ProductSection />
-      <AboutSection />
-      {/* <FooterSection /> */}
-      <ContactSection />
-      <FooterSec />
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 4 }}
+        >
+          <Navbar />
+          <HeroSection />
+          <PerformanceSection />
+          <AddonPerformance />
+          <ProductSection />
+          <AboutSection />
+          {/* <FooterSection /> */}
+          <ContactSection />
+          <FooterSec />
+        </motion.div>
+      </AnimatePresence>
     </>
   );
 };
